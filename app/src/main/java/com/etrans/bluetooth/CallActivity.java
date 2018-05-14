bdc87ab1524ff871d30b7461ed7b8634200d0f96
王楠魁 <wnk@e-trans.com.cn>
2018-05-14 19:57:12 +0800

    1.来电、接听、拨出监听，做三个功能实现处理 2.数据库整合，方法调用，添加首字母字段，按首字母排序通讯录 3.拨号功能完善 4.挂断逻辑处理 5.蓝牙音乐信息获取不到问题修复 6.音乐信息bug调试 7.获取蓝牙设置信息(自动连接自动接听状态，设备名，pin号) 8.通话记录数据回调并显示去电、来电、未接来电列表，获取类型bug修复 9.通话界面布局编辑 10.来电界面布局编辑，功能实现 11.自动获取通讯录

--- app/src/main/java/com/etrans/bluetooth/CallActivity.java
+++ app/src/main/java/com/etrans/bluetooth/CallActivity.java
@@ -1,14 +1,14 @@
 package com.etrans.bluetooth;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.RemoteException;
 import android.os.SystemClock;
+import android.telephony.PhoneNumberUtils;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Chronometer;
@@ -19,219 +19,210 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.etrans.bluetooth.Goc.GocsdkCallbackImp;
-import com.etrans.bluetooth.db.GocDatabase;
-import com.etrans.bluetooth.event.BackgroundCurrentNumberEvent;
-import com.etrans.bluetooth.event.BackgroundHfpStatusEvent;
-import com.etrans.bluetooth.event.CurrentNumberEvent;
-import com.etrans.bluetooth.event.HfpStatusEvent;
-
-import org.greenrobot.eventbus.EventBus;
-import org.greenrobot.eventbus.Subscribe;
-import org.greenrobot.eventbus.ThreadMode;
+import com.etrans.bluetooth.db.Database;
+import com.etrans.bluetooth.utils.ToastFactory;
 
 public class CallActivity extends Activity implements OnClickListener {
-	public static boolean running = false;
-
-	private RelativeLayout rl_call_page;
-	private RelativeLayout rl_talking_page;
-	private TextView tv_calling_name;
+	public static final int MSG_CALL_STATUS = 0;// 拨出电话
+	public static final int Msg_CONNECT = 1;// 接通
+	public static final int MSG_INCOMING_HANGUP = 2;// 拒接
+	private RelativeLayout rl_call_pager;
+	private RelativeLayout rl_connect;
+	private TextView tv_call_people_name;
 	private Chronometer chronometer;
-	private TextView tv_talking_number;
+	private TextView tv_connection_info;
 	private LinearLayout ll_number;
 	private ImageView iv_number;
 	private ImageView iv_guaduan;
 	private ImageView iv_qieshengdao;
 	private ImageView iv_bujingyin;
 
-    // flag
+	// flag
 	private boolean isShowNumber = false;
+	//private boolean isPhoneConnection = false;
+	//private boolean isInComing = false;
 	private boolean volume_flag = false;
-	private boolean isMute = true;
+	private boolean isMute = false;
+	private boolean isConnect = false;
+	
+	private String callNumber;//前台拨号
+	//private String calloutNumber;//后台拨号
+	
+	private static Handler hand = null;
+	private Handler handler = new Handler() {
+		private String phoneNumber;
 
-	private String currentNumber = "";
-	private int currentStatus = HfpStatus.IDLE;
-
-	private TextView tv_calling_number;
-	private TextView tv_talking_name;
-
+		public void handleMessage(android.os.Message msg) {
+			switch (msg.what) {
+			case MSG_CALL_STATUS:
+				phoneNumber = (String) msg.obj;
+				callOut(phoneNumber);
+				break;
+			case MSG_INCOMING_HANGUP:
+				System.out.println("handler------");
+				chronometer.stop();
+				finish();
+				break;
+			case Msg_CONNECT:
+				System.out.println("callNumber你是不是为空啊？"+callNumber);
+				if(callNumber!=null){
+					System.out.println("我都拨出去了，你接了没有哇");
+					callConnect(callNumber);
+				}else{
+					Handler handler2 = InComingActivity.getHandler();
+					if(handler2!=null){
+						handler2.sendEmptyMessage(InComingActivity.MSG_INCOMING_CONNECTION);
+					}
+				}
+				break;
+			}
+		};
+	};
+	private String incomingNumber;
+	private ImageView iv_one;
+	private ImageView iv_two;
+	private ImageView iv_three;
+	private ImageView iv_four;
+	private ImageView iv_five;
+	private ImageView iv_six;
+	private ImageView iv_seven;
+	private ImageView iv_eight;
+	private ImageView iv_nine;
+	private ImageView iv_xinghao;
+	private ImageView iv_zero;
+	private ImageView iv_jinghao;
+	public static Handler getHandler() {
+		return hand;
+	}
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_call);
 		initView();
 		initData();
-
-        EventBus.getDefault().register(this);
-
-        running = true;
+		isConnect();
+		hand = handler;
 	}
 
-	@Override
-	protected void onDestroy() {
-        EventBus.getDefault().unregister(this);
-		super.onDestroy();
-
-        running = false;
-	}
-
-	@Subscribe(sticky = true,threadMode = ThreadMode.MAIN)
-	public void onEvent(CurrentNumberEvent event){
-        currentNumber = event.number;
-        String name = GocDatabase.getDefault().getNameByNumber(currentNumber);
-        if (TextUtils.isEmpty(name)) {
-            name = "未知联系人";
-        }
-
-        tv_calling_name.setText(name);
-        tv_talking_name.setText(name);
-        tv_calling_number.setText(currentNumber);
-        tv_talking_number.setText(currentNumber);
-
-        EventBus.getDefault().removeStickyEvent(event);
-	}
-
-	@Subscribe(sticky = true,threadMode = ThreadMode.MAIN)
-	public void onEvent(BackgroundCurrentNumberEvent event){
-		currentNumber = event.number;
-		String name = GocDatabase.getDefault().getNameByNumber(currentNumber);
-		if (TextUtils.isEmpty(name)) {
-			name = "未知联系人";
+	private void isConnect() {
+		if(isConnect){
+			rl_call_pager.setVisibility(View.GONE);
+			rl_connect.setVisibility(View.VISIBLE);
+			if(callNumber!=null){
+				tv_connection_info.setText(callNumber);
+			}
+			if(incomingNumber!=null){
+				tv_connection_info.setText(incomingNumber);
+			}
+			chronometer.setFormat("%s");
+			chronometer.setBase(SystemClock.elapsedRealtime());// 复位键
+			chronometer.start();
+			
+		}else{
+			rl_call_pager.setVisibility(View.VISIBLE);
+			rl_connect.setVisibility(View.GONE);
+			if(callNumber!=null){
+				tv_call_people_name.setText(callNumber);
+			}
+			/*if(calloutNumber!=null){
+				tv_call_people_name.setText(calloutNumber);
+				callNumber = null;
+			}	*/
 		}
-
-		tv_calling_name.setText(name);
-		tv_talking_name.setText(name);
-		tv_calling_number.setText(currentNumber);
-		tv_talking_number.setText(currentNumber);
-
-		EventBus.getDefault().removeStickyEvent(event);
 	}
 
-	@Subscribe(sticky = true,threadMode = ThreadMode.MAIN)
-	public void onEvent(HfpStatusEvent event){
-		Log.d("CallActivity","status:"+event.status);
-		if(event.status <= HfpStatus.CONNECTED){
-			currentNumber = "";
-			finish();
-        }else if(event.status == HfpStatus.CALLING){
-            onCalling();
-		}else if(event.status == HfpStatus.TALKING){
-            onTalking();
-		}
 
-        EventBus.getDefault().removeStickyEvent(event);
+
+
+
+	protected void callOut(String phoneNumber) {
+		placeCall(phoneNumber);
+		rl_call_pager.setVisibility(View.VISIBLE);
+		rl_connect.setVisibility(View.GONE);
+		tv_call_people_name.setText(phoneNumber);
 	}
 
-	@Subscribe(sticky = true,threadMode = ThreadMode.MAIN)
-	public void onEvent(BackgroundHfpStatusEvent event){
-		if(event.status <= HfpStatus.CONNECTED){
-			currentNumber = "";
-			finish();
-		}else if(event.status == HfpStatus.CALLING){
-			onCalling();
-		}else if(event.status == HfpStatus.TALKING){
-			onTalking();
-		}
 
-		EventBus.getDefault().removeStickyEvent(event);
-	}
 
-	private void onTalking(){
-		rl_call_page.setVisibility(View.GONE);
-		rl_talking_page.setVisibility(View.VISIBLE);
 
-        if(!TextUtils.isEmpty(currentNumber)) {
-            tv_talking_number.setText(currentNumber);
 
-            String name = GocDatabase.getDefault().getNameByNumber(currentNumber);
-            if (!TextUtils.isEmpty(name)) {
-                tv_talking_name.setText(name);
-            } else {
-                tv_talking_name.setText("未知联系人");
-            }
-        }else{
-            tv_talking_number.setText("");
-            tv_talking_name.setText("未知联系人");
-        }
-
+	protected void callConnect(String callNumber2) {
+		rl_call_pager.setVisibility(View.GONE);
+		rl_connect.setVisibility(View.VISIBLE);
+		
+		tv_connection_info.setText(callNumber2);
+		
 		chronometer.setFormat("%s");
 		chronometer.setBase(SystemClock.elapsedRealtime());// 复位键
 		chronometer.start();
 	}
 
-	private void onCalling(){
-        rl_call_page.setVisibility(View.VISIBLE);
-        rl_talking_page.setVisibility(View.GONE);
-
-        if(!TextUtils.isEmpty(currentNumber)){
-            tv_calling_number.setText(currentNumber);
-            String name = GocDatabase.getDefault().getNameByNumber(currentNumber);
-            if(!TextUtils.isEmpty(name)){
-                tv_calling_name.setText(name);
-            }else{
-                tv_calling_name.setText("未知联系人");
-            }
-        }else{
-            tv_calling_number.setText("");
-            tv_calling_name.setText("未知联系人");
-        }
-    }
-
 	private void initData() {
 		Intent intent = getIntent();
-		currentNumber = intent.getStringExtra("number");
-		Log.d("app","initData currentNumber:"+ currentNumber);
-        currentStatus  = intent.getIntExtra("status",0);
-
-        if(currentStatus <= HfpStatus.CONNECTED){
-            finish();
-        }else if(currentStatus == HfpStatus.CALLING){
-            onCalling();
-        }else if(currentStatus == HfpStatus.TALKING){
-            onTalking();
-        }
+		callNumber = intent.getStringExtra("callNumber");
+		System.out.println("CallActivity中拨出电话号"+callNumber);
+		incomingNumber = intent.getStringExtra("incomingNumber");
+		//calloutNumber = intent.getStringExtra("calloutNumber");
+		isConnect  = intent.getBooleanExtra("isConnect", false);
+		if(!TextUtils.isEmpty(callNumber)){
+			SQLiteDatabase database = Database.getSystemDb();
+			Database.createTable(database, Database.Sql_create_phonebook_tab);
+			String phoneName = Database.queryPhoneName(database,
+					Database.PhoneBookTable, callNumber);
+			if (TextUtils.isEmpty(phoneName)) {
+				tv_call_people_name.setText(callNumber);
+				System.out.println("没有名字的号码");
+			} else {
+				tv_call_people_name.setText(phoneName);
+			}
+			database.close();
+		}
 	}
 
-	public static void start(Context context,int status, String number){
-        if(running)return;
-        running = true;
-
-        Intent intent = new Intent(context, CallActivity.class);
-        intent.putExtra("number",number);
-        intent.putExtra("status",status);
-		intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK );
-        context.startActivity(intent);
-    }
-
-
 	private void initView() {
-		rl_call_page = (RelativeLayout) findViewById(R.id.rl_call_page);
-		tv_calling_name = (TextView) findViewById(R.id.tv_call_name);
-		tv_calling_number = (TextView) findViewById(R.id.tv_call_number);
+		rl_call_pager = (RelativeLayout) findViewById(R.id.rl_call_pager);
+		tv_call_people_name = (TextView) findViewById(R.id.tv_call_people_name);
 
-		rl_talking_page = (RelativeLayout) findViewById(R.id.rl_talking_page);
+		rl_connect = (RelativeLayout) findViewById(R.id.rl_connect);
 		chronometer = (Chronometer) findViewById(R.id.chronometer);
-		tv_talking_number = (TextView) findViewById(R.id.tv_connection_number);
-		tv_talking_name = (TextView) findViewById(R.id.tv_connection_name);
+		tv_connection_info = (TextView) findViewById(R.id.tv_connection_info);
+		
+		
+		ll_number = (LinearLayout) findViewById(R.id.ll_number);
 
-		ll_number = (LinearLayout) findViewById(R.id.ll_numpad);
 		iv_number = (ImageView) findViewById(R.id.iv_number);
 		iv_guaduan = (ImageView) findViewById(R.id.iv_guaduan);
 		iv_qieshengdao = (ImageView) findViewById(R.id.iv_qieshengdao);
 		iv_bujingyin = (ImageView) findViewById(R.id.iv_bujingyin);
 		
 		
-		findViewById(R.id.iv_one).setOnClickListener(this);
-		findViewById(R.id.iv_two).setOnClickListener(this);
-		findViewById(R.id.iv_three).setOnClickListener(this);
-		findViewById(R.id.iv_four).setOnClickListener(this);
-		findViewById(R.id.iv_five).setOnClickListener(this);
-		findViewById(R.id.iv_six).setOnClickListener(this);
-		findViewById(R.id.iv_seven).setOnClickListener(this);
-		findViewById(R.id.iv_eight).setOnClickListener(this);
-		findViewById(R.id.iv_nine).setOnClickListener(this);
-		findViewById(R.id.iv_xinghao).setOnClickListener(this);
-		findViewById(R.id.iv_zero).setOnClickListener(this);
-		findViewById(R.id.iv_jinghao).setOnClickListener(this);
+		iv_one = (ImageView) findViewById(R.id.iv_one);
+		iv_two = (ImageView) findViewById(R.id.iv_two);
+		iv_three = (ImageView) findViewById(R.id.iv_three);
+		iv_four = (ImageView) findViewById(R.id.iv_four);
+		iv_five = (ImageView) findViewById(R.id.iv_five);
+		iv_six = (ImageView) findViewById(R.id.iv_six);
+		iv_seven = (ImageView) findViewById(R.id.iv_seven);
+		iv_eight = (ImageView) findViewById(R.id.iv_eight);
+		iv_nine = (ImageView) findViewById(R.id.iv_nine);
+		iv_xinghao = (ImageView) findViewById(R.id.iv_xinghao);
+		iv_zero = (ImageView) findViewById(R.id.iv_zero);
+		iv_jinghao = (ImageView) findViewById(R.id.iv_jinghao);
+		
+		
+		iv_one.setOnClickListener(this);
+		iv_two.setOnClickListener(this);
+		iv_three.setOnClickListener(this);
+		iv_four.setOnClickListener(this);
+		iv_five.setOnClickListener(this);
+		iv_six.setOnClickListener(this);
+		iv_seven.setOnClickListener(this);
+		iv_eight.setOnClickListener(this);
+		iv_nine.setOnClickListener(this);
+		iv_xinghao.setOnClickListener(this);
+		iv_zero.setOnClickListener(this);
+		iv_jinghao.setOnClickListener(this);
 
 		iv_number.setOnClickListener(this);
 		iv_guaduan.setOnClickListener(this);
@@ -242,14 +233,14 @@ public class CallActivity extends Activity implements OnClickListener {
 	@Override
 	public void onClick(View v) {
 		switch (v.getId()) {
-		case R.id.iv_number:
+		case R.id.iv_number: 
 			showNumber();
 			break;
 		case R.id.iv_guaduan:
 			hangUp();
 			break;
 		case R.id.iv_qieshengdao:
-			if (GocsdkCallbackImp.hfpStatus >= 3) {
+			if (GocsdkCallbackImp.hfpStatus > 0) {
 				switchCarAndphone();
 			} else {
 				Toast.makeText(this, "请您先连接设备", Toast.LENGTH_SHORT).show();
@@ -259,71 +250,43 @@ public class CallActivity extends Activity implements OnClickListener {
 			switchMute();
 			break;
 		case R.id.iv_one:
-			phoneDTMFCode('1');
 			break;
 		case R.id.iv_two:
-			phoneDTMFCode('2');
 			break;
 		case R.id.iv_three:
-			phoneDTMFCode('3');
 			break;
 		case R.id.iv_four:
-			phoneDTMFCode('4');
 			break;
 		case R.id.iv_five:
-			phoneDTMFCode('5');
 			break;
 		case R.id.iv_six:
-			phoneDTMFCode('6');
 			break;
 		case R.id.iv_seven:
-			phoneDTMFCode('7');
 			break;
 		case R.id.iv_eight:
-			phoneDTMFCode('8');
 			break;
 		case R.id.iv_nine:
-			phoneDTMFCode('9');
 			break;
 		case R.id.iv_xinghao:
-			phoneDTMFCode('*');
 			break;
 		case R.id.iv_zero:
-			phoneDTMFCode('0');
 			break;
 		case R.id.iv_jinghao:
-			phoneDTMFCode('#');
 			break;
 		}
 	}
-	private void phoneDTMFCode(char code){
-		try {
-			MainActivity.getService().phoneTransmitDTMFCode(code);
-		} catch (RemoteException e) {
-			e.printStackTrace();
-		}
-	}
+
 	private void switchMute() {
-		Handler handler = MainActivity.getHandler();
+		MainActivity activity = new MainActivity();
+		Handler handler = activity.getHandler();
 		isMute = !isMute;
 		if(isMute){
 			handler.sendEmptyMessage(MainActivity.MSG_SET_MICPHONE_OFF);
 			iv_bujingyin.setImageResource(R.drawable.btn_jianpan_bujingyin_selector);
-//			try {
-//				MainActivity.getService().muteOpenAndClose(1);
-//			} catch (RemoteException e) {
-//				e.printStackTrace();
-//			}
 		}else{
 			handler.sendEmptyMessage(MainActivity.MSG_SET_MICPHONE_ON);
 			iv_bujingyin.setImageResource(R.drawable.btn_jianpan_jingyin_selector);
-//			try {
-//				MainActivity.getService().muteOpenAndClose(0);
-//			} catch (RemoteException e) {
-//				e.printStackTrace();
-//			}
 		}
-
 	}
 	// 切换声音在车机端与手机端
 		private void switchCarAndphone() {
@@ -334,14 +297,14 @@ public class CallActivity extends Activity implements OnClickListener {
 				} catch (RemoteException e1) {
 					e1.printStackTrace();
 				}
-				Toast.makeText(this, "手机端", Toast.LENGTH_SHORT).show();
+				ToastFactory.showToast(this,"手机端");
 			} else {// 车机端
 				try {
 					MainActivity.getService().phoneTransferBack();
 				} catch (RemoteException e1) {
 					e1.printStackTrace();
 				}
-				Toast.makeText(this, "车机端", Toast.LENGTH_SHORT).show();
+				ToastFactory.showToast(this,"车机端");
 			}
 		}
 	private void showNumber() {
@@ -349,20 +312,36 @@ public class CallActivity extends Activity implements OnClickListener {
 		if (isShowNumber) {
 			ll_number.setVisibility(View.VISIBLE);
 		} else {
-			ll_number.setVisibility(View.GONE);
+			ll_number.setVisibility(View.INVISIBLE);
 		}
 	}
 
 	// 挂断
 	private void hangUp() {
+		
+		//Toast.makeText(this, "挂断", 0).show();
 		try {
 			MainActivity.getService().phoneHangUp();
 		} catch (RemoteException e) {
 			e.printStackTrace();
 		}
+		
 	}
+	
+	// 拨打正确的电话
+		private static void placeCall(String mLastNumber) {
+			if (mLastNumber.length() == 0)
+				return;
+			if (PhoneNumberUtils.isGlobalPhoneNumber(mLastNumber)) {
+				if (mLastNumber == null || !TextUtils.isGraphic(mLastNumber)) {
+					return;
+				}
+				try {
+					MainActivity.getService().phoneDail(mLastNumber);
+				} catch (RemoteException e) {
+					e.printStackTrace();
+				}
+			}
+		}
 
-    @Override
-    public void onBackPressed() {
-    }
 }
